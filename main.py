7ec3cee7643171f37a5f1e426a21317d35a0db81
yeshjho <yeshjho@naver.com>
2025-05-16 21:25:00 +0900

    Discord bot primal setup

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+import asyncio
+import logging
+from logging.handlers import RotatingFileHandler
 import os
-import random
-import unicodedata
+
+import discord
+
+
+class DiscordBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        print('hi')
+
+    async def on_message(self, message: discord.Message):
+        if not message.author.bot:
+            logging.getLogger('discord_bot_raspi').log(logging.INFO, message.content)
+            await message.channel.send(message.content)
 
 
 if __name__ == '__main__':
+    handler = RotatingFileHandler(
+        filename='logs/discord_bot_raspi.log',
+        encoding='utf-8',
+        maxBytes=32,
+        backupCount=5,
+    )
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger('discord_bot_raspi')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
     import django
     django.setup()
 
-    from db.dictionary.models import *
+    bot = DiscordBot(intents=discord.Intents.all())
 
-    count = HanjaUsage.objects.count()
-    while input('> ') != 'q':
-        usage = HanjaUsage.objects.all()[random.randint(0, count - 1)]
-        print(usage.word.word)
-        for sense in KoreanWordSense.objects.filter(word=usage.word):
-            print('\t' + sense.definition)
-        input(': ')
-        print(usage.word.hanja)
+    bot.run(os.getenv('DISCORD_BOT_KEY', 'error'))
